ba9d4f6b401921cad37cc44dced27ac146658466
aniroodh-ravikumar <ar765@cornell.edu>
2018-11-23 17:40:49 -0500

    Extracted data from train

--- classifier.py
+++ classifier.py
@@ -1,7 +1,25 @@
-
+import csv
 
 def main():
-	print("Sad")
+
+	tweetDict = {}
+
+	with open('train.csv','r') as trainFile:
+		trainReader = csv.reader(trainFile, delimiter = '\n')
+		#fields = trainReader.next()
+		for i,tweet in enumerate(trainReader):
+			if not tweet:
+				break
+			if i == 0:
+				continue
+			tweet = tweet[0].strip('[').strip(']').split(',')
+			id, text, favCount, created, retCount, label = tweet[0],tweet[1],tweet[3],tweet[5],tweet[12],tweet[17]
+			tweetDict[id] = [text, favCount, created, retCount, label]
+
+	for tweetId,tweet in tweetDict.items():
+		print(tweetId + ": " + tweet[0])
+
+
 
 if __name__ == '__main__':
 	main()
