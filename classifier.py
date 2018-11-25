56585c305fb4724a20c2867c89d9257b9fdbc53f
aniroodh-ravikumar <ar765@cornell.edu>
2018-11-25 16:41:23 -0500

    Finally put things into functions

--- classifier.py
+++ classifier.py
@@ -6,134 +6,116 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from sklearn.linear_model import RidgeClassifierCV
 from sklearn.svm import LinearSVC
 
-def main():
-
+def readData(fName, isTraining):
 	tweetDict = {}
 	wordDict = {}
 	c = 0
-
-	with open('train.csv','r') as trainFile:
-		trainReader = csv.reader(trainFile, delimiter = '\n')
-		#fields = trainReader.next()
-		for i,tweet in enumerate(trainReader):
-			if not tweet:
-				break
-			if i == 0:
-				continue
-			tweet = tweet[0].strip('[').strip(']').split(',')
-			id, text, favCount, created, retCount, label = tweet[0],tweet[1],tweet[3],tweet[5],tweet[12],tweet[17]
-			words = nltk.word_tokenize(text)
-			for w in words:
-				if not(w.lower() in wordDict):
-					wordDict[w.lower()] = c
-					c += 1
-			tweetDict[id] = [text, favCount, created, retCount, label, words]
-
-	XTr = np.zeros((len(tweetDict), c+11))
-	YTr = np.zeros(len(tweetDict))
-	for i,(tweetId,tweet) in enumerate(tweetDict.items()):
-		tWords = tweet[-1]
-		YTr[i] = tweet[-2]
-		for tw in tWords:
-			if tw.lower() in wordDict:
-				XTr[i][wordDict[tw.lower()]] += 1
-			else:
-				XTr[i][c] += 1
-		tStr = tweet[0]
-		if tStr.find("\"") != -1:
-			XTr[i][c+1] = 1
-		if tStr.find("https") != -1:
-			XTr[i][c+2] = 1
-		if tStr.find("#") != -1:
-			XTr[i][c+3] = 1
-		sid = SentimentIntensityAnalyzer()
-		sentiments = sid.polarity_scores(tStr)
-		XTr[i][c+4] = round(sentiments['pos'])
-		XTr[i][c+5] = round(sentiments['neg'])
-		XTr[i][c+6] = round(sentiments['neu'])
-		s = tweet[2]
-		hr = ((s.split(' '))[1].split(':'))[0]
-		if hr in range(0,10):
-			XTr[i][c+7] = 1
-		elif hr in range(10,17):
-			XTr[i][c+8] = 1
-		elif hr in range(17,21):
-			XTr[i][c+9] = 1
-		else:
-			XTr[i][c+10] = 1
-
-
-
-	testTweetDict = {}
 	idList = []
 
-	with open('test.csv','r') as testFile:
-		testReader = csv.reader(testFile, delimiter = '\n')
-		#fields = trainReader.next()
-		for i,tweet in enumerate(testReader):
+	with open(fName,'r') as tFile:
+		tReader = csv.reader(tFile, delimiter = '\n')
+		for i,tweet in enumerate(tReader):
 			if not tweet:
 				break
 			if i == 0:
 				continue
 			tweet = tweet[0].strip('[').strip(']').split(',')
-			id, text, favCount, created, retCount = tweet[0],tweet[1],tweet[3],tweet[5],tweet[12]
+			id, text, favCount, created, retCount = tweet[0],tweet[1],
+			tweet[3],tweet[5],tweet[12]
+			if isTraining:
+				label = tweet[17]
 			words = nltk.word_tokenize(text)
-			idList.append(id)
-			testTweetDict[id] = [text, favCount, created, retCount, words]
 
-	XTe = np.zeros((len(testTweetDict), c+11))
-	for i,(tweetId,tweet) in enumerate(testTweetDict.items()):
+			if isTraining:
+				for w in words:
+					if not(w.lower() in wordDict):
+						wordDict[w.lower()] = c
+						c += 1
+				tweetDict[id] = [text, favCount, created, retCount, label, words]
+			else:
+				idList.append(id)
+				tweetDict[id] = [text, favCount, created, retCount, words]
+
+	if isTraining:
+		return tweetDict,wordDict,c
+	else:
+		return tweetDict, idList
+
+def createX(tweetDict, wordDict, c):
+
+	X = np.zeros((len(tweetDict), c+11))
+	for i,(tweetId,tweet) in enumerate(tweetDict.items()):
 		tWords = tweet[-1]
 		for tw in tWords:
 			if tw.lower() in wordDict:
-				XTe[i][wordDict[tw.lower()]] += 1
+				X[i][wordDict[tw.lower()]] += 1
 			else:
-				XTe[i][c] += 1
+				X[i][c] += 1
 		tStr = tweet[0]
 		if tStr.find("\"") != -1:
-			XTe[i][c+1] = 1
+			X[i][c+1] = 1
 		if tStr.find("https") != -1:
-			XTe[i][c+2] = 1
+			X[i][c+2] = 1
 		if tStr.find("#") != -1:
-			XTe[i][c+3] = 1
+			X[i][c+3] = 1
 		sid = SentimentIntensityAnalyzer()
 		sentiments = sid.polarity_scores(tStr)
-		XTe[i][c+4] = round(sentiments['pos'])
-		XTe[i][c+5] = round(sentiments['neg'])
-		XTe[i][c+6] = round(sentiments['neu'])
+		X[i][c+4] = round(sentiments['pos'])
+		X[i][c+5] = round(sentiments['neg'])
+		X[i][c+6] = round(sentiments['neu'])
 		s = tweet[2]
 		hr = ((s.split(' '))[1].split(':'))[0]
 		if hr in range(0,10):
-			XTe[i][c+7] = 1
+			X[i][c+7] = 1
 		elif hr in range(10,17):
-			XTe[i][c+8] = 1
+			X[i][c+8] = 1
 		elif hr in range(17,21):
-			XTe[i][c+9] = 1
+			X[i][c+9] = 1
 		else:
-			XTe[i][c+10] = 1
+			X[i][c+10] = 1
 
+	return X
 
-	# fiftyAvg = 0
-	# for i in range(100):
-	# 	indices = np.array(range(len(tweetDict)))
-	# 	np.random.shuffle(indices)
-	# 	XTr = XTr[indices]
-	# 	YTr = YTr[indices]
-	# 	XTr80 = XTr[:int(0.8*len(tweetDict))]
-	# 	YTr80 = YTr[:int(0.8*len(tweetDict))]
-	# 	XTr20 = XTr[int(0.8*len(tweetDict)):]
-	# 	YTr20 = YTr[int(0.8*len(tweetDict)):]
-	#
-	# 	clf = LinearSVC(random_state=0, tol=1e-5)
-	# 	#clf = RidgeClassifierCV(alphas=[1e-3, 1e-2, 1e-1, 1]).fit(XTr80, YTr80)
-	# 	#clf = MultinomialNB()
-	# 	clf.fit(XTr80, YTr80)
-	# 	preds = clf.predict(XTr20)
-	# 	temp = (np.equal(preds,YTr20))
-	# 	fiftyAvg += (np.sum(temp)/temp.shape)
-	# fiftyAvg/=100
-	#
-	# print(fiftyAvg)
+def createYTr(tweetDict):
+
+	YTr = np.array([tweet[-2] for (tweetId,tweet) in tweetDict.items()])
+	return YTr
+
+def getFiftyAvg(tweetDict, XTr, YTr):
+
+	fiftyAvg = 0
+	for i in range(50):
+		indices = np.array(range(len(tweetDict)))
+		np.random.shuffle(indices)
+		XTr = XTr[indices]
+		YTr = YTr[indices]
+		XTr80 = XTr[:int(0.8*len(tweetDict))]
+		YTr80 = YTr[:int(0.8*len(tweetDict))]
+		XTr20 = XTr[int(0.8*len(tweetDict)):]
+		YTr20 = YTr[int(0.8*len(tweetDict)):]
+	
+		clf = LinearSVC(random_state=0, tol=1e-5)
+		#clf = RidgeClassifierCV(alphas=[1e-3, 1e-2, 1e-1, 1]).fit(XTr80, YTr80)
+		#clf = MultinomialNB()
+		clf.fit(XTr80, YTr80)
+		preds = clf.predict(XTr20)
+		temp = (np.equal(preds,YTr20))
+		fiftyAvg += (np.sum(temp)/temp.shape)
+	fiftyAvg/=50
+
+	return fiftyAvg
+
+def main():
+
+	tweetDict,wordDict,c = readData('train.csv',True)
+	XTr = createX(tweetDict, wordDict, c)
+	YTr = createYTr(tweetDict)
+
+	testTweetDict, idList = readData('test.csv',False)
+	XTe = createX(testTweetDict, wordDict, c)
+
+	fiftyAvg = getFiftyAvg(tweetDict, XTr, YTr)
+	print(fiftyAvg)
 
 	 # clf = MultinomialNB()
 	 # clf.fit(XTr80, YTr80)
@@ -160,7 +142,7 @@ def main():
 	#temp = (np.equal(preds,YTr20))
 	#print(np.sum(temp)/temp.shape)
 
-	with open('outputMark4.csv', 'w') as testfile:
+	with open('outputMark4.5.csv', 'w') as testfile:
 		filewriter = csv.writer(testfile, delimiter=',')
 		filewriter.writerow(['ID','Label'])
 		for i, (id,pred) in enumerate(zip(idList,preds)):
