e4b1c81373069ec27102a3dd26846c8aacea7a2c
Karan Newatia <kn348@cornell.edu>
2018-11-26 00:09:01 -0500

    extratrees

--- classifier.py
+++ classifier.py
@@ -6,7 +6,8 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from sklearn.linear_model import RidgeClassifierCV
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_selection import VarianceThreshold
+from sklearn.ensemble import ExtraTreesClassifier
+
 
 def readData(fName, isTraining):
 	tweetDict = {}
@@ -90,35 +91,34 @@ def main():
 
 	testTweetDict, idList = readData('test.csv',False)
 	XTe = createX(testTweetDict, wordDict, c)
-	# sel = VarianceThreshold(threshold=(.95 * (1 - .95)))
-	# sel.fit_transform(XTr)
 
-	fiftyAvg = 0
-	for i in range(100):
-		indices = np.array(range(len(tweetDict)))
-		np.random.shuffle(indices)
-		XTr = XTr[indices]
-		YTr = YTr[indices]
-
-		XTr80 = XTr[:int(0.8*len(tweetDict))]
-		YTr80 = YTr[:int(0.8*len(tweetDict))]
-
-		XTr20 = XTr[int(0.8*len(tweetDict)):]
-		YTr20 = YTr[int(0.8*len(tweetDict)):]
-
-		clf = RandomForestClassifier(n_estimators=int(np.sqrt(len(XTr80[0]))), max_depth=100, random_state=0)
-		#clf = LinearSVC(random_state=0, tol=1e-5)
-	 	#clf = RidgeClassifierCV(alphas=[1e-3, 1e-2, 1e-1, 1]).fit(XTr80, YTr80)
-	 	#clf = MultinomialNB()
-
-		clf.fit(XTr80, YTr80)
-		preds = clf.predict(XTr20)
-
-		#temp = np.equal(preds,YTr20)
-		#fiftyAvg += (np.sum(temp)/preds.shape)
-		fiftyAvg += (np.sum(preds==YTr20)/preds.shape)
-	fiftyAvg/=100
-	print(fiftyAvg)
+	# fiftyAvg = 0
+	# for i in range(100):
+	# 	indices = np.array(range(len(tweetDict)))
+	# 	np.random.shuffle(indices)
+	# 	XTr = XTr[indices]
+	# 	YTr = YTr[indices]
+	#
+	# 	XTr80 = XTr[:int(0.8*len(tweetDict))]
+	# 	YTr80 = YTr[:int(0.8*len(tweetDict))]
+	#
+	# 	XTr20 = XTr[int(0.8*len(tweetDict)):]
+	# 	YTr20 = YTr[int(0.8*len(tweetDict)):]
+	#
+	# 	#clf = RandomForestClassifier(n_estimators=int(np.sqrt(len(XTr80[0]))), max_depth=100, random_state=0)
+	# 	clf = ExtraTreesClassifier(n_estimators=int(np.sqrt(len(XTr80[0]))), max_depth=100, random_state=0)
+	# 	#clf = LinearSVC(random_state=0, tol=1e-5)
+	#  	#clf = RidgeClassifierCV(alphas=[1e-3, 1e-2, 1e-1, 1]).fit(XTr80, YTr80)
+	#  	#clf = MultinomialNB()
+	#
+	# 	clf.fit(XTr80, YTr80)
+	# 	preds = clf.predict(XTr20)
+	#
+	# 	#temp = np.equal(preds,YTr20)
+	# 	#fiftyAvg += (np.sum(temp)/preds.shape)
+	# 	fiftyAvg += (np.sum(preds==YTr20)/preds.shape)
+	# fiftyAvg/=100
+	# print(fiftyAvg)
 
 	 # clf = MultinomialNB()
 	 # clf.fit(XTr80, YTr80)
@@ -130,17 +130,17 @@ def main():
 
 	#clf = RidgeClassifierCV(alphas=[1e-3, 1e-2, 1e-1, 1]).fit(XTr, YTr)
 
-	# clf = RandomForestClassifier(n_estimators=int(np.sqrt(len(XTr[0]))), max_depth=100, random_state=0)
-	# # #clf = LinearSVC(random_state=0, tol=1e-5)
-	# clf.fit(XTr, YTr)
-	#
-	# preds = clf.predict(XTe)
+	clf = RandomForestClassifier(n_estimators=int(np.sqrt(len(XTr[0]))), max_depth=100, random_state=0)
+	# #clf = LinearSVC(random_state=0, tol=1e-5)
+	clf.fit(XTr, YTr)
+
+	preds = clf.predict(XTe)
 
 	# counter = 0
 	# for i in preds:
 	# 	if i=='-1':
 	# 		counter+=1
-	# print(preds)
+	# # print(preds)
 	#
 	# print(counter)
 
@@ -148,7 +148,7 @@ def main():
 	#temp = (np.equal(preds,YTr20))
 	#print(np.sum(temp)/temp.shape)
 
-	# with open('outputMark7.csv', 'w') as testfile:
+	# with open('outputMark8.csv', 'w') as testfile:
 	# 	filewriter = csv.writer(testfile, delimiter=',')
 	# 	filewriter.writerow(['ID','Label'])
 	# 	for i, (id,pred) in enumerate(zip(idList,preds)):
