fbb1664d08c72c7ebe14d6343990b0b19fd7c010
Robert Yang <ry92@cornell.edu>
2018-11-28 17:38:01 -0500

    Added hashing to decrease number of features. Tried out gradient boosting as well.

--- classifier.py
+++ classifier.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import RidgeClassifierCV
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
+from sklearn.ensemble import GradientBoostingClassifier
 
 def readData(fName, isTraining):
 	tweetDict = {}
@@ -48,8 +49,9 @@ def createX(tweetDict, wordDict, c):
 		tWords = tweet[-1]
 		for tw in tWords:
 			if tw.lower() in wordDict:
-
 				X[i][wordDict[tw.lower()]] += 1
+				# hv = hash(wordDict[tw.lower()])%256
+				# X[i][v] += 1
 			else:
 
 				X[i][c] += 1
@@ -84,7 +86,7 @@ def createYTr(tweetDict):
 	return YTr
 
 def main():
-
+	#numOfHashedFeatures = 256
 	tweetDict,wordDict,c = readData('train.csv',True)
 	XTr = createX(tweetDict, wordDict, c)
 	YTr = createYTr(tweetDict)
@@ -92,41 +94,43 @@ def main():
 	testTweetDict, idList = readData('test.csv',False)
 	XTe = createX(testTweetDict, wordDict, c)
 
-	fiftyAvg = 0
-	for i in range(100):
-		indices = np.array(range(len(tweetDict)))
-		np.random.shuffle(indices)
-		XTr = XTr[indices]
-		YTr = YTr[indices]
-		XTr80 = XTr[:int(0.8*len(tweetDict))]
-		YTr80 = YTr[:int(0.8*len(tweetDict))]
-		XTr20 = XTr[int(0.8*len(tweetDict)):]
-		YTr20 = YTr[int(0.8*len(tweetDict)):]
-		#clf = LinearSVC(random_state=0, tol=1e-5)
-	 	#clf = RidgeClassifierCV(alphas=[1e-3, 1e-2, 1e-1, 1]).fit(XTr80, YTr80)
-		clf = RandomForestClassifier(n_estimators=int(np.sqrt(len(XTr80[0]))), max_depth=100, random_state=0)
-		clf.fit(XTr80, YTr80)
-		# model = SelectFromModel(clf, threshold=1e-4, prefit=True)
-		# XTr80 = model.transform(XTr80)
-		# idx = model.get_support(True)
-		# XTr20 = XTr20[:,idx]
-		# clf = RandomForestClassifier(n_estimators=int(np.sqrt(len(XTr80[0]))), max_depth=100, random_state=0)
-		# clf.fit(XTr80, YTr80)
-		preds = clf.predict(XTr20)
-		#temp = np.equal(preds,YTr20)
-		#fiftyAvg += (np.sum(temp)/preds.shape)
-		fiftyAvg += (np.sum(preds==YTr20)/preds.shape)
-	fiftyAvg/=100
-	print(fiftyAvg)
+	# fiftyAvg = 0
+	# for i in range(10):
+	# 	indices = np.array(range(len(tweetDict)))
+	# 	np.random.shuffle(indices)
+	# 	XTr = XTr[indices]
+	# 	YTr = YTr[indices]
+	# 	XTr80 = XTr[:int(0.8*len(tweetDict))]
+	# 	YTr80 = YTr[:int(0.8*len(tweetDict))]
+	# 	XTr20 = XTr[int(0.8*len(tweetDict)):]
+	# 	YTr20 = YTr[int(0.8*len(tweetDict)):]
+	# 	#clf = LinearSVC(random_state=0, tol=1e-5)
+	#  	#clf = RidgeClassifierCV(alphas=[1e-3, 1e-2, 1e-1, 1]).fit(XTr80, YTr80)
+	# 	# clf = RandomForestClassifier(n_estimators=int(np.sqrt(len(XTr80[0]))), max_depth=100, random_state=0)
+	# 	clf = GradientBoostingClassifier(n_estimators=100, learning_rate=0.025, max_depth=3, random_state=0).fit(XTr80, YTr80)
+	# 	clf.fit(XTr80, YTr80)
+	# 	# model = SelectFromModel(clf, threshold=1e-4, prefit=True)
+	# 	# XTr80 = model.transform(XTr80)
+	# 	# idx = model.get_support(True)
+	# 	# XTr20 = XTr20[:,idx]
+	# 	# clf = RandomForestClassifier(n_estimators=int(np.sqrt(len(XTr80[0]))), max_depth=100, random_state=0)
+	# 	# clf.fit(XTr80, YTr80)
+	# 	preds = clf.predict(XTr20)
+	# 	#temp = np.equal(preds,YTr20)
+	# 	#fiftyAvg += (np.sum(temp)/preds.shape)
+	# 	fiftyAvg += (np.sum(preds==YTr20)/preds.shape)
+	# fiftyAvg/=10
+	# print(fiftyAvg)
 
 
 	#clf = RidgeClassifierCV(alphas=[1e-3, 1e-2, 1e-1, 1]).fit(XTr, YTr)
-
 	# clf = RandomForestClassifier(n_estimators=int(np.sqrt(len(XTr[0]))), max_depth=100, random_state=0)
-	# #clf = LinearSVC(random_state=0, tol=1e-5)
-	#
-	# clf.fit(XTr, YTr)
-	# preds = clf.predict(XTe)
+	clf = GradientBoostingClassifier(n_estimators=100, learning_rate=0.1, max_depth=3, random_state=0).fit(XTr, YTr)
+	# # #clf = LinearSVC(random_state=0, tol=1e-5)
+	# #
+
+	clf.fit(XTr, YTr)
+	preds = clf.predict(XTe)
 
 	# counter = 0
 	# for i in preds:
@@ -140,11 +144,11 @@ def main():
 	#temp = (np.equal(preds,YTr20))
 	#print(np.sum(temp)/temp.shape)
 
-	# with open('outputMark9.csv', 'w') as testfile:
-	# 	filewriter = csv.writer(testfile, delimiter=',')
-	# 	filewriter.writerow(['ID','Label'])
-	# 	for i, (id,pred) in enumerate(zip(idList,preds)):
-	# 		filewriter.writerow([id,int(pred)])
+	with open('outputMark14csv', 'w') as testfile:
+		filewriter = csv.writer(testfile, delimiter=',')
+		filewriter.writerow(['ID','Label'])
+		for i, (id,pred) in enumerate(zip(idList,preds)):
+			filewriter.writerow([id,int(pred)])
 
 if __name__ == '__main__':
 	main()
