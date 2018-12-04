f4da046168768917cea553cabd29a54d8e119ea0
Karan Newatia <kn348@cornell.edu>
2018-12-04 18:37:23 -0500

    hashing 256 submission

--- classifier.py
+++ classifier.py
@@ -50,11 +50,11 @@ def createX(tweetDict, wordDict, c):
 		for tw in tWords:
 			if tw.lower() in wordDict:
 				#X[i][wordDict[tw.lower()]] += 1
-				hv = hash(wordDict[tw.lower()])%c
+				v = hash(wordDict[tw.lower()])%c
 				X[i][v] += 1
 			else:
 				X[i][c] += 1
-				
+
 		tStr = tweet[0]
 		if tStr.find("\"") != -1:
 			X[i][c+1] = 1
@@ -87,7 +87,7 @@ def createYTr(tweetDict):
 
 def main():
 	# Change this boolean when you're alternating between testing 80 20 and generating a full submission
-	generatingSubmission = False
+	generatingSubmission = True
 
 	#numOfHashedFeatures = 256
 	c = 256
@@ -99,33 +99,33 @@ def main():
 	XTe = createX(testTweetDict, wordDict, c)
 
 	if not generatingSubmission:
-		# fiftyAvg = 0
-		# for i in range(10):
-		# 	indices = np.array(range(len(tweetDict)))
-		# 	np.random.shuffle(indices)
-		# 	XTr = XTr[indices]
-		# 	YTr = YTr[indices]
-		# 	XTr80 = XTr[:int(0.8*len(tweetDict))]
-		# 	YTr80 = YTr[:int(0.8*len(tweetDict))]
-		# 	XTr20 = XTr[int(0.8*len(tweetDict)):]
-		# 	YTr20 = YTr[int(0.8*len(tweetDict)):]
-		# 	#clf = LinearSVC(random_state=0, tol=1e-5)
-		#  	#clf = RidgeClassifierCV(alphas=[1e-3, 1e-2, 1e-1, 1]).fit(XTr80, YTr80)
-		# 	# clf = RandomForestClassifier(n_estimators=int(np.sqrt(len(XTr80[0]))), max_depth=100, random_state=0)
-		# 	clf = GradientBoostingClassifier(n_estimators=100, learning_rate=0.025, max_depth=3, random_state=0).fit(XTr80, YTr80)
-		# 	clf.fit(XTr80, YTr80)
-		# 	# model = SelectFromModel(clf, threshold=1e-4, prefit=True)
-		# 	# XTr80 = model.transform(XTr80)
-		# 	# idx = model.get_support(True)
-		# 	# XTr20 = XTr20[:,idx]
-		# 	# clf = RandomForestClassifier(n_estimators=int(np.sqrt(len(XTr80[0]))), max_depth=100, random_state=0)
-		# 	# clf.fit(XTr80, YTr80)
-		# 	preds = clf.predict(XTr20)
-		# 	#temp = np.equal(preds,YTr20)
-		# 	#fiftyAvg += (np.sum(temp)/preds.shape)
-		# 	fiftyAvg += (np.sum(preds==YTr20)/preds.shape)
-		# fiftyAvg/=10
-		# print(fiftyAvg)
+		fiftyAvg = 0
+		for i in range(10):
+			indices = np.array(range(len(tweetDict)))
+			np.random.shuffle(indices)
+			XTr = XTr[indices]
+			YTr = YTr[indices]
+			XTr80 = XTr[:int(0.8*len(tweetDict))]
+			YTr80 = YTr[:int(0.8*len(tweetDict))]
+			XTr20 = XTr[int(0.8*len(tweetDict)):]
+			YTr20 = YTr[int(0.8*len(tweetDict)):]
+			#clf = LinearSVC(random_state=0, tol=1e-5)
+		 	#clf = RidgeClassifierCV(alphas=[1e-3, 1e-2, 1e-1, 1]).fit(XTr80, YTr80)
+			# clf = RandomForestClassifier(n_estimators=int(np.sqrt(len(XTr80[0]))), max_depth=100, random_state=0)
+			clf = GradientBoostingClassifier(n_estimators=100, learning_rate=0.025, max_depth=3, random_state=0).fit(XTr80, YTr80)
+			clf.fit(XTr80, YTr80)
+			# model = SelectFromModel(clf, threshold=1e-4, prefit=True)
+			# XTr80 = model.transform(XTr80)
+			# idx = model.get_support(True)
+			# XTr20 = XTr20[:,idx]
+			# clf = RandomForestClassifier(n_estimators=int(np.sqrt(len(XTr80[0]))), max_depth=100, random_state=0)
+			# clf.fit(XTr80, YTr80)
+			preds = clf.predict(XTr20)
+			#temp = np.equal(preds,YTr20)
+			#fiftyAvg += (np.sum(temp)/preds.shape)
+			fiftyAvg += (np.sum(preds==YTr20)/preds.shape)
+		fiftyAvg/=10
+		print(fiftyAvg)
 
 	else:
 		#clf = RidgeClassifierCV(alphas=[1e-3, 1e-2, 1e-1, 1]).fit(XTr, YTr)
@@ -137,23 +137,12 @@ def main():
 		clf.fit(XTr, YTr)
 		preds = clf.predict(XTe)
 
-		with open('outputMark14csv', 'w') as testfile:
-		filewriter = csv.writer(testfile, delimiter=',')
-		filewriter.writerow(['ID','Label'])
-		for i, (id,pred) in enumerate(zip(idList,preds)):
-			filewriter.writerow([id,int(pred)])
+		with open('outputMarkHashing.csv', 'w') as testfile:
+			filewriter = csv.writer(testfile, delimiter=',')
+			filewriter.writerow(['ID','Label'])
+			for i, (id,pred) in enumerate(zip(idList,preds)):
+				filewriter.writerow([id,int(pred)])
 
-	# counter = 0
-	# for i in preds:
-	# 	if i=='-1':
-	# 		counter+=1
-	# # print(preds)
-	#
-	# print(counter)
-
-	#print(preds)
-	#temp = (np.equal(preds,YTr20))
-	#print(np.sum(temp)/temp.shape)
 
 if __name__ == '__main__':
 	main()
