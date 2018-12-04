3324013b21c8f916a3dd098faf936c078b162fd7
Robert Yang <ry92@cornell.edu>
2018-12-04 18:11:20 -0500

    First push with pos tagging

--- classifier.py
+++ classifier.py
@@ -10,6 +10,7 @@ from sklearn.feature_selection import SelectFromModel
 def readData(fName, isTraining):
 	tweetDict = {}
 	wordDict = {}
+	tupDict = {}
 	c = 0
 	idList = []
 
@@ -26,34 +27,45 @@ def readData(fName, isTraining):
 				label = tweet[17]
 			words = nltk.word_tokenize(text)
 
+			#pos_tags = nltk.pos_tag(words)
+
+
 			if isTraining:
 				for w in words:
 					if not(w.lower() in wordDict):
 						wordDict[w.lower()] = c
 						c += 1
+				# for tup in pos_tags:
+				# 	if not (tup in tupDict):
+				# 		tupDict[tup] = c
+				# 		c +=1
+
 				tweetDict[id] = [text, favCount, created, retCount, label, words]
+				# tweetDict[id] = [text, favCount, created, retCount, label, pos_tags]
 			else:
 				idList.append(id)
 				tweetDict[id] = [text, favCount, created, retCount, words]
+				# tweetDict[id] = [text, favCount, created, retCount, pos_tags]
 
 	if isTraining:
-		return tweetDict,wordDict,c
+		# return tweetDict,wordDict,c
+		return tweetDict, tupDict, c
 	else:
 		return tweetDict, idList
 
 def createX(tweetDict, wordDict, c):
 
-	X = np.zeros((len(tweetDict), c+11))
+	X = np.zeros((len(tweetDict), c+11+35))
 	for i,(tweetId,tweet) in enumerate(tweetDict.items()):
 		tWords = tweet[-1]
 		for tw in tWords:
 			if tw.lower() in wordDict:
-
+			#if tw in wordDict:
 				X[i][wordDict[tw.lower()]] += 1
-			else:
+				#X[i][wordDict[tw]] += 1
 
+			else:
 				X[i][c] += 1
-        pos_tags = nltk.pos_tag()
 		
 
 		tStr = tweet[0]
@@ -79,6 +91,27 @@ def createX(tweetDict, wordDict, c):
 		else:
 			X[i][c+10] = 1
 
+		pos_tags = nltk.pos_tag(tWords)
+		categoryDict = {}
+		categories = ['CC', 'CD', 'DT', 'EX', 'FW', 'IN', 'JJ', 'JJR', 'JJS', 'LS', 'MD', 'NN', 'NNS', 'NNP', 'NNPS', 'PDT', 'POS', 'PRP', 'PRP$', 'RB', 'RBR', 'RBS', 'RP', 'TO', 'UH', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ', 'WDT', 'WP', 'WP$', 'WRB']
+		for cat in categories:
+			categoryDict[cat] = 0
+		for word, cat in pos_tags:
+				if cat in categories:
+					categoryDict[cat] += 1
+
+		# tempCounter = c+10
+		# for cat, count in categoryDict.items():
+		# 	tempCounter += 1
+		# 	X[i][tempCounter] = count
+
+		# totNumOfFeatures = tempCounter+1
+
+		# newX = np.zeros((len(tweetDict), totNumOfFeatures))
+
+		# for vector in range((len(tweetDict))):
+		# 	newX[vector] = X[vector,:totNumOfFeatures]
+
 	return X
 
 def createYTr(tweetDict):
@@ -87,7 +120,8 @@ def createYTr(tweetDict):
 	return YTr
 
 def main():
-	generatingSubmission = false
+	# Change this boolean when you're alternating between testing 80 20 and generating a full submission
+	generatingSubmission = False
 
 	tweetDict,wordDict,c = readData('train.csv',True)
 	XTr = createX(tweetDict, wordDict, c)
@@ -108,8 +142,10 @@ def main():
 			XTr20 = XTr[int(0.8*len(tweetDict)):]
 			YTr20 = YTr[int(0.8*len(tweetDict)):]
 			#clf = LinearSVC(random_state=0, tol=1e-5)
-		 	#clf = RidgeClassifierCV(alphas=[1e-3, 1e-2, 1e-1, 1]).fit(XTr80, YTr80)
-			clf = RandomForestClassifier(n_estimators=int(np.sqrt(len(XTr80[0]))), max_depth=100, random_state=0)
+			#clf = RidgeClassifierCV(alphas=[1e-3, 1e-2, 1e-1, 1]).fit(XTr80, YTr80)
+			rowDim, columnDim = XTr80.shape
+
+			clf = RandomForestClassifier(n_estimators=int(np.sqrt(columnDim)), max_depth=100, random_state=0)
 			clf.fit(XTr80, YTr80)
 			# model = SelectFromModel(clf, threshold=1e-4, prefit=True)
 			# XTr80 = model.transform(XTr80)
@@ -132,7 +168,7 @@ def main():
 		clf.fit(XTr, YTr)
 		preds = clf.predict(XTe)
 
-		with open('outputMark9.csv', 'w') as testfile:
+		with open('outputMark16.csv', 'w') as testfile:
 			filewriter = csv.writer(testfile, delimiter=',')
 			filewriter.writerow(['ID','Label'])
 			for i, (id,pred) in enumerate(zip(idList,preds)):
